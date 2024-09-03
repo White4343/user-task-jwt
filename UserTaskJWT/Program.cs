942cce3e0713d15b9209b3112146d88cf4fcbd2e
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-03 13:58:15 +0300

    Feat - adding service, functions and classes for AppDbContext

--- UserTaskJWT/Program.cs
+++ UserTaskJWT/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using UserTaskJWT.Web.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DockerConnectionString")));
 
 var app = builder.Build();
 
@@ -15,4 +21,23 @@ app.MapGet("/love", () => "Hello World!").WithOpenApi();
 
 app.UseHttpsRedirection();
 
+CreateDbIfNotExists(app);
+
 await app.RunAsync().ConfigureAwait(false);
+
+void CreateDbIfNotExists(IHost host)
+{
+    using var scope = host.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+
+        DbInitialize.InitializeAsync(context).Wait();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
