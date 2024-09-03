52e42b1808b0f6e48e484d91d0dd1d370ed77d59
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-03 13:49:10 +0300

    Feat - AppDbContext & EntityConfigurations

---
+++ UserTaskJWT/Data/EntityConfigurations/TaskEntityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace UserTaskJWT.Web.Api.Data.EntityConfigurations
+{
+    public class TaskEntityConfiguration : IEntityTypeConfiguration<Tasks.Task>
+    {
+        public void Configure(EntityTypeBuilder<Tasks.Task> builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title).IsRequired();
+
+            builder.Property(t => t.Status).IsRequired();
+
+            builder.Property(t => t.Priority).IsRequired();
+
+            builder.Property(t => t.CreatedAt).IsRequired();
+
+            builder.Property(t => t.UpdatedAt).IsRequired();
+
+            builder.HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId)
+                .IsRequired();
+        }
+    }
+}
