ea34f605901f906d845f63fb751203d9bbe9179e
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-05 17:33:13 +0300

    Feat - add Middleware

---
+++ UserTaskJWT/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using Newtonsoft.Json;
+
+namespace UserTaskJWT.Web.Api.Middleware
+{
+    public class ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context);
+
+            try
+            {
+                await next(context).ConfigureAwait(false);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                await CreateContext(context, e, 401).ConfigureAwait(false);
+            }
+            catch (BadHttpRequestException e)
+            {
+                await CreateContext(context, e, 400).ConfigureAwait(false);
+            }
+            catch (ValidationException e)
+            {
+                await CreateContext(context, e, 400).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                await CreateContext(context, e, 500).ConfigureAwait(false);
+            }
+        }
+
+        private async Task CreateContext(HttpContext context, Exception e, int statusCode)
+        {
+            logger.LogError(e,"{Message}", e.Message);
+
+            var result = JsonConvert.SerializeObject(new
+            {
+                error = e.Message
+            });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsync(result).ConfigureAwait(false);
+        }
+    }
+}
