b65d7135970b1dd026ac70671fc838eebb690e55
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-06 10:45:34 +0300

    Feat - add TaskRepository

---
+++ UserTaskJWT/Tasks/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace UserTaskJWT.Web.Api.Tasks
+{
+    public interface ITaskRepository
+    {
+        Task<Task> CreateAsync(Task task, CancellationToken cancellationToken);
+        Task<IEnumerable<Task>> GetTasksByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task<Task> GetTaskAsync(Guid id, CancellationToken cancellationToken);
+        Task<Task> UpdateAsync(Task task, CancellationToken cancellationToken);
+        Task<Task> DeleteAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ UserTaskJWT/Tasks/TaskRepository.cs
@@ -0,0 +1,109 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserTaskJWT.Web.Api.Data;
+
+namespace UserTaskJWT.Web.Api.Tasks
+{
+    public class TaskRepository(AppDbContext context, ILogger<TaskRepository> logger) : ITaskRepository
+    {
+        public async Task<Task> CreateAsync(Task task, CancellationToken cancellationToken)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(task);
+                ArgumentNullException.ThrowIfNull(context.Tasks);
+
+                await context.Tasks.AddAsync(task, cancellationToken).ConfigureAwait(false);
+                await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+                logger.LogInformation("Task created: {Task} by user {UserId}", task, task.UserId);
+
+                return task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Task>> GetTasksByUserIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(userId);
+                ArgumentNullException.ThrowIfNull(context.Tasks);
+
+                var tasks = await context.Tasks.Where(t => t.UserId == userId).ToListAsync(cancellationToken).ConfigureAwait(false);
+
+                return tasks;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<Task> GetTaskAsync(Guid id, CancellationToken cancellationToken)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(id);
+                ArgumentNullException.ThrowIfNull(context.Tasks);
+
+                var task = await context.Tasks.FirstOrDefaultAsync(t => t.Id == id, cancellationToken)
+                    .ConfigureAwait(false);
+
+                ArgumentNullException.ThrowIfNull(task);
+
+                return task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<Task> UpdateAsync(Task task, CancellationToken cancellationToken)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(task);
+                ArgumentNullException.ThrowIfNull(context.Tasks);
+
+                context.Tasks.Update(task);
+                await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+                return task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<Task> DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(id);
+                ArgumentNullException.ThrowIfNull(context.Tasks);
+
+                var task = await GetTaskAsync(id, cancellationToken).ConfigureAwait(false);
+
+                context.Tasks.Remove(task);
+                await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+                return task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
