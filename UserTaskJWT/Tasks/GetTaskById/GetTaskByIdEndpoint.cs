28ad8c8bc72ede894fb4929dae05285e62441b55
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-06 19:47:35 +0300

    Feat - add GetTaskById

---
+++ UserTaskJWT/Tasks/GetTaskById/GetTaskByIdEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using UserTaskJWT.Web.Api.Endpoints;
+
+namespace UserTaskJWT.Web.Api.Tasks.GetTaskById
+{
+    public class GetTaskByIdEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("tasks/{id}",
+                    async (HttpContext httpContext, Guid id, GetTaskByIdHandler useCase,
+                            ClaimsPrincipal user) =>
+                        await useCase.HandleAsync(id, user, httpContext.RequestAborted).ConfigureAwait(false))
+                .WithTags("Tasks")
+                .RequireAuthorization()
+                .WithOpenApi();
+        }
+    }
+}
