4a8929c8a79df104ce3f8900cfab97b6b401b422
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-06 10:45:54 +0300

    Feat - add CreateTask

---
+++ UserTaskJWT/Tasks/CreateTask/CreateTaskEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using UserTaskJWT.Web.Api.Endpoints;
+
+namespace UserTaskJWT.Web.Api.Tasks.CreateTask
+{
+    public class CreateTaskEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("tasks", 
+                    async (HttpContext httpContext, CreateTaskCommand request, CreateTaskHandler useCase) => 
+                        await useCase.HandleAsync(request, httpContext.RequestAborted).ConfigureAwait(false))
+                .WithTags("Tasks")
+                .WithOpenApi();
+        }
+    }
+}
