9146ddf987d93807de911bae98644bca38874bd9
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-06 21:52:09 +0300

    Feat - add UpdateTask

---
+++ UserTaskJWT/Tasks/UpdateTask/UpdateTaskEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using UserTaskJWT.Web.Api.Endpoints;
+
+namespace UserTaskJWT.Web.Api.Tasks.UpdateTask
+{
+    public class UpdateTaskEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/tasks/{id}", 
+                async (HttpContext httpContent, Guid id, UpdateTaskCommand command, UpdateTaskHandler useCase, 
+                        ClaimsPrincipal user) => 
+                    await useCase.HandleAsync(id, command, user, httpContent.RequestAborted).ConfigureAwait(false))
+                .WithTags("Tasks")
+                .RequireAuthorization()
+                .WithOpenApi();
+        }
+    }
+}
---
+++ UserTaskJWT/Tasks/UpdateTask/UpdateTaskValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace UserTaskJWT.Web.Api.Tasks.UpdateTask
+{
+    public class UpdateTaskValidator : AbstractValidator<UpdateTaskCommand>
+    {
+        public UpdateTaskValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                    .WithMessage("Title is required");
+
+            RuleFor(x => x.DueDate)
+                .Must(date => date != default(DateTime))
+                    .WithMessage("DueDate must be in DateTime format");
+
+            RuleFor(x => x.Status)
+                .IsInEnum();
+
+            RuleFor(x => x.Priority)
+                .IsInEnum();
+        }
+    }
+}
