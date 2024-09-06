bed493b67008010b203928f9eec2e2019ec494c7
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-09-06 22:41:28 +0300

    Feat - add DeleteTask

---
+++ UserTaskJWT/Tasks/DeleteTask/DeleteTaskEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using UserTaskJWT.Web.Api.Endpoints;
+
+namespace UserTaskJWT.Web.Api.Tasks.DeleteTask
+{
+    public class DeleteTaskEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("tasks/{id}",
+                async (HttpContext httpContent, Guid id, DeleteTaskHandler useCase, ClaimsPrincipal user) =>
+                    await useCase.HandleAsync(id, user, httpContent.RequestAborted).ConfigureAwait(false))
+                .WithTags("Tasks")
+                .RequireAuthorization()
+                .WithOpenApi();
+        }
+    }
+}
